8e22fe0d2fa5836859d08b140055c35f23ab7b59
elenberg <everettlenberg@gmail.com>
2016-10-08 11:40:08 -0600

    retrying this i promise ill get it to work eventually git is going to kill meeeee

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^sign_up',  TemplateView.as_view('sign_up.html'), name="sign_up"),
-    url(r'^dashboard',  TemplateView.as_view('dashboard.html'), name="dashboard"),
-    url(r'^deck/[\d]+',  TemplateView.as_view('deck.html'), name="deck"),
-    url(r'^note/[\d]+',  TemplateView.as_view('note.html'), name='note'),
-    url(r'^courses',  TemplateView.as_view('courses.html'), name='courses'),
-    url(r'^poll',  TemplateView.as_view('poll.html'), name='poll'),
-    url(r'^create_poll',  TemplateView.as_view('create_poll.html'), name='create_poll'),
-    url(r'^',  TemplateView.as_view('home.html'), name='home'),
+    url(r'^sign_up', TemplateView.as_view(template_name='sign_up.html')),
+    url(r'^dashboard', TemplateView.as_view(template_name='dashboard.html')),
+    url(r'^deck/[\d]+', TemplateView.as_view(template_name='deck.html')),
+    url(r'^note/[\d]+', TemplateView.as_view(template_name='note.html')),
+    url(r'^courses', TemplateView.as_view(template_name='courses.html')),
+    url(r'^poll', TemplateView.as_view(template_name='poll.html')),
+    url(r'^create_poll', TemplateView.as_view(template_name='create_poll.html')),
+    url(r'^', TemplateView.as_view(template_name='home.html')),
 ]
