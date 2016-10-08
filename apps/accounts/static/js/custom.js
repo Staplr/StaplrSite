39d7e2b8ed7210d910944b50fc632c2d3b879497
Mauriel <mauriel.ramirez@gmail.com>
2016-10-08 17:56:45 -0600

    rip

--- apps/accounts/static/js/custom.js
+++ apps/accounts/static/js/custom.js
@@ -2,49 +2,71 @@
  * Created by Mauriel on 10/7/2016.
  */
 $(document).ready(function () {
-$(':file').on('fileselect', function(event, numFiles, label) {
+    $(':file').on('fileselect', function (event, numFiles, label) {
         console.log(numFiles);
         console.log(label);
     });
-// Update User Object
 
+    // Update User Object
+    if (localStorage.getItem('user') != null) {
+        var user = JSON.parse(localStorage.getItem('user'));
+        data_item = {'id': user.id}
+        $.ajax({
+            url: api + 'user/',
+            method: "POST",
+            data: data_item,
+            success: function (result) {
+                user = JSON.stringify(result);
+                localStorage.setItem('user', user);
+                $("#login-dropdown").hide();
 
-if(localStorage.getItem('user') != null){
-    var user = JSON.parse(localStorage.getItem('user'))
-    data_item = {'id':user.id}
-    $.ajax({url: api + 'user/',
-                method: "POST",
-                data: data_item,
-                success: function(result){
-                    user = JSON.stringify(result);
-                    localStorage.setItem('user', user);
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                  console.log(textStatus, errorThrown);
-                },
-                failure: function(result){
-                    message = JSON.parse(result);
-                    console.log(message);
-                }
+                $("#user-info-dropdown > a").text(user.username);
+                $("#user-info-dropdown").show();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+                localStorage.clear();
+                $("#login-dropdown").show();
+                $("#user-info-dropdown").hide();
+            },
+        });
+    }
+
+    $(document).on('change', ':file', function () {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
     });
-}
-
-$(document).on('change', ':file', function() {
-    var input = $(this),
-        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-    input.trigger('fileselect', [numFiles, label]);
-});
 
 });
 
-function getUserObject(user_id){
-   return $.ajax({
+$("#btn-login").click(function () {
+    var datadict = $("#login-form").serialize();
+    $.ajax({
+        url: api + '/login_user/',
+        data: datadict,
+        method: "POST",
+        success: function (result) {
+            user = JSON.stringify(result);
+            localStorage.setItem('user', user);
+            $("#login-dropdown").hide();
+            $("#user-info-dropdown").show();
+        },
+        error: function (result) {
+            localStorage.clear();
+            $("#login-dropdown").show();
+            $("#user-info-dropdown").hide();
+        }
+    })
+});
+
+function getUserObject(user_id) {
+    return $.ajax({
         url: api + 'user/',
         data: {'id': user_id},
-        success(result){
+        success: function (result) {
             user_object = result;
         },
-
     })
 }
