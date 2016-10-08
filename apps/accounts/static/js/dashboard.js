c3fe1f42c270addd6f3381e6a9fd7e1593d3661f
Mauriel <mauriel.ramirez@gmail.com>
2016-10-08 13:42:49 -0600

    Setting up dashboard

--- apps/accounts/static/js/dashboard.js
+++ apps/accounts/static/js/dashboard.js
@@ -1,8 +1,6 @@
 /**
  * Created by Mauriel on 10/8/2016.
  */
-
 $(document).ready(function () {
-    $('.parallax-window').parallax({imageSrc: 'static/img/bannerImg.jpg'});
+    $(".card").flip();
 });
-
